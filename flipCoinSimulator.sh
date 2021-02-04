c8500eeaf57c838d294496bad01e66b5218296de
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-04 20:52:44 +0530

    Added code to simulate the winner for a single flipping of a coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo "Welcome to Flip Coin Simulator Problem!"
+
+a=$((RANDOM%2))
+
+if [[ $a -eq 0 ]]
+then
+        echo "Winner is : Heads"
+else
+        echo "Winner is : Tails"
+fi
